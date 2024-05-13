a19199d1212bf7075d28bd3462e5eee971b5ae89
Sigurd <ridderpultost1@gmail.com>
2024-05-13 12:15:48 +0200

    smalll changes

--- EksamenQ5/Car.cs
+++ EksamenQ5/Car.cs
@@ -7,10 +7,9 @@ public class Car
     public int Milage { get; set; }
     public decimal Price { get; set; }
 
-    // Parameterløs konstruktør som CsvHelper kan bruke
+    // Parameterløs konstruktør for CsvHelper.
     public Car() { }
-
-    // Konstruktør med parametere som du allerede hadde
+    
     public Car(string model, int year, string color, int milage, decimal price)
     {
         Model = model;
--- EksamenQ5/CarService.cs
+++ EksamenQ5/CarService.cs
@@ -1,8 +1,5 @@
 using CsvHelper;
 using System.Globalization;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using CsvHelper.Configuration;
 
